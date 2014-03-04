25d5b09f412ba19c8810d163dbf43ef30c865367
Mathis Klooß <m.klooss@loewenstark.com>
2014-03-04 11:24:42 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,49 @@
+Loewenstark_MageSetupEmail
+===================
+required FireGento_MageSetup
+The Module provides the email templates for MageSetup.
+
+Info
+-----------
+Do not import the Mail Templates with the MageSetup installer.
+
+the Templates files will be load from the directory from the following directories
+
+* app/design/frontend/PACKAGE/THEME/locale/LOCALE_CODE/$TYPE/FILE
+* app/design/frontend/PACKAGE/default/locale/LOCALE_CODE/$TYPE/FILE
+* app/locale/LOCALE_CODE/template/magesetupemail/$TYPE/FILE
+* default folders from Magento
+
+Attention
+------------
+- If the folder will be changed, you have to revalidate the path.
+
+Requirements
+------------
+- PHP >= 5.3.0
+- FireGento_MageSetup
+
+Compatibility
+-------------
+- Magento >= 1.6
+
+Support
+-------
+If you encounter any problems or bugs, please create an issue on [GitHub](https://github.com/Loewenstark/Loewenstark_MageSetupEmail/issues).
+
+Contribution
+------------
+Any contribution to the development of MageSetup is highly welcome. The best possibility to provide any code is to open a [pull request on GitHub](https://help.github.com/articles/using-pull-requests).
+
+Developer
+---------
+* Mathis Klooss
+
+Licence
+-------
+[GNU General Public License, version 3 (GPLv3)](http://opensource.org/licenses/gpl-3.0)
+
+Copyright
+---------
+(c) 2011-2013 Loewenstark Web-Solutions GmbH
+
---
+++ src/app/code/community/Loewenstark/MageSetupEmail/Model/Core/Translate.php
@@ -0,0 +1,222 @@
+<?php
+
+class Loewenstark_MageSetupEmail_Model_Core_Translate
+extends Mage_Core_Model_Translate
+{
+    // ttl for apc/apcu/xcache
+    const UC_TTL = 604800; //7 days
+    const DEBUG_LOG_FILE = 'translate.log';
+    const LOCALE_FOLDER = 'magesetupemail';
+    
+    protected $_debug = false;
+    protected $_force_revalidate = false;
+    protected $_cache = null;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        if(extension_loaded('apc'))
+        {
+            $this->_cache = 'apc';
+        } elseif(extension_loaded('apcu')) {
+            $this->_cache = 'apcu';
+        } elseif(extension_loaded('xCache')) {
+            $this->_cache = 'xcache';
+        }
+    }
+
+    /**
+     * Retrive translated template file
+     * LWS: replace the hole method and added cache mechanism, Files: magesetup or templates based
+     *
+     * @param string $file
+     * @param string $type
+     * @param string $localeCode
+     * @return string file content
+     */
+    public function getTemplateFile($file, $type, $localeCode=null)
+    {
+        $package = $this->getConfig(self::CONFIG_KEY_DESIGN_PACKAGE);
+        $theme = $this->getConfig(self::CONFIG_KEY_DESIGN_THEME);
+        $store_id = $this->getConfig(self::CONFIG_KEY_STORE);
+        
+        if (is_null($localeCode) || preg_match('/[^a-zA-Z_]/', $localeCode)) {
+            $localeCode = $this->getLocale();
+        }
+        
+        // cache key
+        $key = 'email_'.md5($file).'_'.$type.'_'.$localeCode.'_'.$package.'_'.$theme.'_'.md5(BP).'_'.$store_id;
+        $cache = $this->_getTemplateCache($key);
+        if($cache)
+        {
+            return $this->_readTemplateFile($cache, false);
+        }
+        
+        $filePath = Mage::getBaseDir('design')  . DS . 'frontend' . DS .
+                $package . DS . $theme . DS. 'locale'. DS . $localeCode . DS . $type . DS . $file;
+        if($this->_FileExists($filePath))
+        {
+            return $this->_readTemplateFile($filePath, $key);
+        }
+        
+        $filePath = Mage::getBaseDir('design')  . DS . 'frontend' . DS .
+                $package . DS . 'default' . DS. 'locale'. DS . $localeCode . DS . $type . DS . $file;
+        
+        if($this->_FileExists($filePath))
+        {
+            return $this->_readTemplateFile($filePath, $key);
+        }
+        
+        $filePath = Mage::getBaseDir('locale') . DS
+                . $localeCode
+                . DS . 'template' . DS . self::LOCALE_FOLDER . DS . $type . DS . $file;
+        
+        if($this->_FileExists($filePath))
+        {
+            return $this->_readTemplateFile($filePath, $key);
+        }
+        
+        $filePath = Mage::getBaseDir('locale') . DS
+                . Mage::app()->getLocale()->getDefaultLocale()
+                . DS . 'template' . DS . self::LOCALE_FOLDER . DS . $type . DS . $file;
+        
+        if(Mage::app()->getLocale()->getDefaultLocale() != $localeCode && $this->_FileExists($filePath))
+        {
+            return $this->_readTemplateFile($filePath, $key);
+        }
+        
+        $filePath = Mage::getBaseDir('locale') . DS
+                . Mage_Core_Model_Locale::DEFAULT_LOCALE
+                . DS . 'template' . DS . self::LOCALE_FOLDER . DS . $type . DS . $file;
+        
+        if(Mage_Core_Model_Locale::DEFAULT_LOCALE != $localeCode && $this->_FileExists($filePath))
+        {
+            return $this->_readTemplateFile($filePath, $key);
+        }
+        
+        // orig part
+        $filePath = Mage::getBaseDir('locale')  . DS
+                  . $localeCode . DS . 'template' . DS . $type . DS . $file;
+        if($this->_FileExists($filePath))
+        {
+            return $this->_readTemplateFile($filePath, $key);
+        }
+        // If no template specified for this locale, use store default
+        $filePath = Mage::getBaseDir('locale') . DS
+                  . Mage::app()->getLocale()->getDefaultLocale()
+                  . DS . 'template' . DS . $type . DS . $file;
+        if(Mage::app()->getLocale()->getDefaultLocale() != $localeCode && $this->_FileExists($filePath))
+        {
+            return $this->_readTemplateFile($filePath, $key);
+        }
+
+        // If no template specified as  store default locale, use en_US/Mage_Core_Model_Locale::DEFAULT_LOCALE
+        $filePath = Mage::getBaseDir('locale') . DS
+                  . Mage_Core_Model_Locale::DEFAULT_LOCALE
+                  . DS . 'template' . DS . $type . DS . $file;
+
+        return $this->_readTemplateFile($filePath, $key);
+    }
+    
+    /**
+     * read file
+     * 
+     * @param string $file
+     * @param bool $cachekey
+     * @return string file content
+     */
+    protected function _readTemplateFile($file, $cachekey=false)
+    {
+        if($cachekey)
+        {
+            $this->_setTemplateCache($cachekey, $file);
+        }
+        return (string)file_get_contents($file);
+    }
+    
+    /**
+     * 
+     * @param string $key
+     * @return boolean|string
+     */
+    protected function _getTemplateCache($key)
+    {
+        if($this->_force_revalidate)
+        {
+            return false;
+        }
+        $value = null;
+        switch($this->_cache)
+        {
+            case 'apc':
+                $value = apc_fetch($key);
+                break;
+            case 'apcu':
+                $value = apcu_fetch($key);
+                break;
+            case 'xcache':
+                $value = xcache_isset($key) ? xcache_get($key) : null;
+                break;
+        }
+        if(!empty($value))
+        {
+            if(file_exists($value))
+            {
+                if($this->_debug)
+                {
+                    Mage::log('loaded key from cache: '.$key, null, self::DEBUG_LOG_FILE);
+                }
+                return $value;
+            }
+            // clear cache if file does not exits
+            $this->_setTemplateCache($key, null);
+        }
+        return false;
+    }
+    
+    /**
+     * 
+     * @param type $key
+     * @param type $value
+     */
+    protected function _setTemplateCache($key, $value)
+    {
+        $save = false;
+        switch($this->_cache)
+        {
+            case 'apc':
+                apc_store($key, $value, self::UC_TTL);
+                break;
+            case 'apcu':
+                apcu_store($key, $value, self::UC_TTL);
+                break;
+            case 'xcache':
+                xcache_set($key, $value, self::UC_TTL);
+                break;
+        }
+        if($this->_cache)
+        {
+            $save = true;
+        }
+        if($this->_debug && $save)
+        {
+            Mage::log('save key to cache: '.$key, null, self::DEBUG_LOG_FILE);
+        }
+        return true;
+    }
+    
+    /**
+     * alias for file_exists
+     * 
+     * @param string $file
+     * @return bool
+     */
+    protected function _FileExists($file)
+    {
+        if($this->_debug)
+        {
+            Mage::log('File: '.$file, null, self::DEBUG_LOG_FILE);
+        }
+        return @file_exists($file);
+    }
+}
